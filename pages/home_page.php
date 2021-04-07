ba0e578ea9a073886330cee7fe6941677595e6b4
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-04-07 21:54:18 +0800

    create files for pages

--- pages/home_page.php
+++ pages/home_page.php
@@ -1,36 +1,36 @@
-<html>
-    <head>
-        <title>Products</title>
-
-    </head>
-
-    <body>
-
-    <?php
-
-        require_once '../database/database.php';
-
-        $pdo = connect();
-
-        $sql = "SELECT * FROM products";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute();
-    ?>
-
-        <div id="products-container">
-            <?php
-                while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                    echo "<div>";
-                    echo '<img src = "data:image/jpg;base64,' . base64_encode($row['product_thumbnail']) . '" width = "50px" height = "50px"/>';
-                    echo '<h3>' . $row['product_name'] . '</h3>';
-                    echo '<p>' .$row['product_description'] . '</p>';
-                    echo '<p>' . $row['product_price'] . '</p>';
-                    echo '<p' . $row['product_stock'] . '</p>';
-                    echo '<button onclick=click>Add to cart </button>';
-                    echo "</div>";
-                }
-
-            ?>
+<!--<html>-->
+<!--    <head>-->
+<!--        <title>Products</title>-->
+<!---->
+<!--    </head>-->
+<!---->
+<!--    <body>-->
+<!---->
+<!--    --><?php
+//
+//        require_once '../database/database.php';
+//
+//        $pdo = connect();
+//
+//        $sql = "SELECT * FROM products";
+//        $stmt = $pdo->prepare($sql);
+//        $stmt->execute();
+//    ?>
+<!---->
+<!--        <div id="products-container">-->
+<!--            --><?php
+//                while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+//                    echo "<div>";
+//                    echo '<img src = "data:image/jpg;base64,' . base64_encode($row['product_thumbnail']) . '" width = "50px" height = "50px"/>';
+//                    echo '<h3>' . $row['product_name'] . '</h3>';
+//                    echo '<p>' .$row['product_description'] . '</p>';
+//                    echo '<p>' . $row['product_price'] . '</p>';
+//                    echo '<p' . $row['product_stock'] . '</p>';
+//                    echo '<button onclick=click>Add to cart </button>';
+//                    echo "</div>";
+//                }
+//
+//            ?>
 <!---->
 <!--        </div>-->
 <!--    <form method="POST">-->
@@ -45,6 +45,6 @@
 //            alert();
 //        }
 //    ?>
-
-    </body>
-</html>
+<!---->
+<!--    </body>-->
+<!--</html>-->
