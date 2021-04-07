00ef4b5a49eec7dfdff744cc9d8df1eeef7e42ea
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-04-07 22:16:35 +0800

    create html for home page

--- pages/home_page.php
+++ pages/home_page.php
@@ -48,3 +48,32 @@
 <!---->
 <!--    </body>-->
 <!--</html>-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shopping cart</title>
+    <link rel="stylesheet" href="../styles/reset.css">
+    <link rel="stylesheet" href="../styles/home_page.css">
+</head>
+<body>
+<nav>
+    <div id="logo-container">
+        <!-- Logo goes here -->
+        <p>Logo here</p>
+    </div>
+
+    <div class="nav-container">
+        <!-- Display first name if user is logged in -->
+        <p>User first name</p>
+
+        <!-- Display either login/logout -->
+        <a href="login.php">Login</a>
+        <a href="register.php">Register</a>
+    </div>
+</nav>
+</body>
+</html>
