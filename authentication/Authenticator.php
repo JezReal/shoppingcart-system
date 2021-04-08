0192acbdc66934d7a3c5cbe0485d961a65b7ef58
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-04-08 23:01:55 +0800

    implement authentication

---
+++ authentication/Authenticator.php
@@ -0,0 +1,21 @@
+<?php
+require_once "../database/database.php";
+class Authenticator {
+
+
+    public function login($email, $password) {
+        $database = connect();
+
+        $query = $database->prepare("SELECT customer_id, first_name FROM customers WHERE customer_email=:email AND customer_password=:password");
+        $query->bindParam("email", $email);
+        $query->bindParam("password", $password);
+        $query->execute();
+
+        if ($query->rowCount() > 0) {
+           $result = $query->fetch(PDO::FETCH_OBJ);
+           return $result->customer_id;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ authentication/auth.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once("Authenticator.php");
+require_once("../session/session.php");
+
+session_start();
+$auth = new Authenticator();
+$login_error_message = "";
+$register_error_message = "";
+
+$email = trim($_POST["email"]);
+$password = trim($_POST["password"]);
+
+$user_id = $auth->login($email, $password);
+
+if ($_POST["loginButton"]) {
+    if ($user_id > 0) {
+        $_SESSION["user_id"] = $user_id;
+        header("Location: ../pages/home_page.php");
+//        header("Location: success.php");
+    } else {
+        header("Location: ./failure.php");
+    }
+    exit();
+}
