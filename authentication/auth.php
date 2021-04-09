70c557ebc74007cbfd95a6a557ef11604f789a30
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-04-09 10:30:27 +0800

    add error messages when logging in

--- authentication/auth.php
+++ authentication/auth.php
@@ -1,11 +1,10 @@
 <?php
 
 require_once("Authenticator.php");
+require_once("../pages/login.php");
 
 session_start();
 $auth = new Authenticator();
-$login_error_message = "";
-$register_error_message = "";
 
 $email = trim($_POST["email"]);
 $password = trim($_POST["password"]);
@@ -16,9 +15,9 @@ if ($_POST["loginButton"]) {
     if ($user_id > 0) {
         $_SESSION["user_id"] = $user_id;
         header("Location: ../pages/home_page.php");
-//        header("Location: success.php");
     } else {
-        header("Location: ./failure.php");
+        loginError();
+        header("Location: ../pages/login.php");
     }
     exit();
 }
