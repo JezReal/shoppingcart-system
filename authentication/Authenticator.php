4873ecf3f7de92004f16585b1d945d75fd104f06
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-04-09 11:42:26 +0800

    implement get user name

--- authentication/Authenticator.php
+++ authentication/Authenticator.php
@@ -5,7 +5,7 @@ class Authenticator {
     public function login($email, $password) {
         $database = connect();
 
-        $query = $database->prepare("SELECT customer_id, first_name FROM customers WHERE customer_email=:email AND customer_password=:password");
+        $query = $database->prepare("SELECT customer_id FROM customers WHERE customer_email=:email AND customer_password=:password");
         $query->bindParam("email", $email);
         $query->bindParam("password", $password);
         $query->execute();
@@ -13,6 +13,22 @@ class Authenticator {
         if ($query->rowCount() > 0) {
            $result = $query->fetch(PDO::FETCH_OBJ);
            return $result->customer_id;
+        } else {
+            return false;
+        }
+
+    }
+
+    public static function getLoggedInUserFirstname($id) {
+        $database = connect();
+
+        $query = $database->prepare("SELECT first_name FROM customers WHERE customer_id=:id");
+        $query->bindParam("id", $id);
+        $query->execute();
+
+        if ($query->rowCount() > 0) {
+            $result = $query->fetch(PDO::FETCH_OBJ);
+            return $result->first_name;
         } else {
             return false;
         }
