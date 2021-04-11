ee4d039a2d8985f30e680cff058baba9fadd1f53
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-04-12 00:11:34 +0800

    display message when cart is empty + minor changes

--- pages/cart.php
+++ pages/cart.php
@@ -2,6 +2,13 @@
 
 session_start();
 
+require_once("../database/database.php");
+
+$database = connect();
+$query = $database->prepare("SELECT * FROM cart_items WHERE cart_id=:customerId");
+$query->bindParam("customerId", $_SESSION['user_id']);
+$query->execute();
+
 ?>
 
 <!DOCTYPE html>
@@ -10,10 +17,13 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shopping cart</title>
-    <link rel="stylesheet" href="../styles/reset.css">
-    <link rel="stylesheet" href="../styles/header.css">
-    <link rel="stylesheet" href="../styles/cart.css">
+    <title>Cart</title>
+
+    <style>
+        <?php include "../styles/reset.css"?>
+        <?php include "../styles/header.css"?>
+        <?php include "../styles/cart.css"?>
+    </style>
 </head>
 <body>
 <nav>
@@ -46,110 +56,119 @@ session_start();
     </div>
 </nav>
 
-    <?php
+<?php
 
-    require_once("../database/database.php");
-    $costumerID=$_SESSION["user_id"];
+require_once("../database/database.php");
+$costumerID = $_SESSION["user_id"];
 
-    $pdo = connect();
-    $sql = "SELECT products.product_thumbnail, products.product_id, products.product_name, products.product_weight, carts.cart_id, cart_items.quantity, products.product_price
+$pdo = connect();
+$sql = "SELECT products.product_thumbnail, products.product_id, products.product_name, products.product_weight, carts.cart_id, cart_items.quantity, products.product_price
             FROM carts JOIN cart_items ON carts.cart_id=cart_items.cart_id
             JOIN customers ON carts.customer_id=customers.customer_id
             JOIN products ON cart_items.product_id = products.product_id
             WHERE customers.customer_id='$costumerID'";
-    $statement = $pdo->prepare($sql);
-    $statement->execute();
-    ?>
+$statement = $pdo->prepare($sql);
+$statement->execute();
+?>
 
 <section>
+    <?php
+    if ($query->rowCount() == 0) {
+        ?>
 
-    <h1>This is the cart page</h1>
+        <h1 id="cart-empty">Cart is empty :((</h1>
 
+        <?php
+    } else {
+        ?>
+        <div id="cart_items_holder">
 
-    <div id="cart_items_holder">
+            <table style="width:100%">
+                <tr>
+                    <th class="header_info">Item</th>
+                    <th class="header_info">Quantity</th>
+                    <th class="header_info">Unit Price</th>
+                    <th class="header_info">Price</th>
+                    <th class="header_info">Action</th>
 
-        <table style="width:100%">
-            <tr>
-                <th class="header_info">Item</th>
-                <th class="header_info">Quantity</th>
-                <th class="header_info">Unit Price</th>
-                <th class="header_info">Price</th>
-                <th class="header_info">Action</th>
-
-            </tr>
+                </tr>
 
                 <?php
 
-                $totalPrice=0;
-                $totalQuantity=0;
-                $totalWeight=0;
+                $totalPrice = 0;
+                $totalQuantity = 0;
+                $totalWeight = 0;
 
                 while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
 
-                    $weight=$row['product_weight'];
-                    $quantity=$row['quantity'];
-                    $unitPrice=$row['product_price'];
-                    $_cartItemWeight=$quantity*$weight;
-                    $price=$quantity*$unitPrice;
-                    $totalPrice+=$price;
-                    $totalQuantity+=$quantity;
-                    $totalWeight+=$_cartItemWeight;
+                    $weight = $row['product_weight'];
+                    $quantity = $row['quantity'];
+                    $unitPrice = $row['product_price'];
+                    $_cartItemWeight = $quantity * $weight;
+                    $price = $quantity * $unitPrice;
+                    $totalPrice += $price;
+                    $totalQuantity += $quantity;
+                    $totalWeight += $_cartItemWeight;
 
-                    echo'<tr>';
-                    echo'<td class="item_column"><img id="thumbnailHolder" src = "' . $row['product_thumbnail'] . '"width = "50px" height = "50px">'. $row['product_name'] .'</td>';
-                    echo'<td class="info_column">'.$quantity.'</td>';
-                    echo'<td class="info_column">'."₱ ".number_format($unitPrice, 2).'</td>';
-                    echo'<td class="info_column">'."₱ ".number_format($price, 2).'</td>';
-                    echo'<td class="remove_button">
+                    echo '<tr>';
+                    echo '<td class="item_column"><img id="thumbnailHolder" src = "' . $row['product_thumbnail'] . '"width = "50px" height = "50px">' . $row['product_name'] . '</td>';
+                    echo '<td class="info_column">' . $quantity . '</td>';
+                    echo '<td class="info_column">' . "₱ " . number_format($unitPrice, 2) . '</td>';
+                    echo '<td class="info_column">' . "₱ " . number_format($price, 2) . '</td>';
+                    echo '<td class="remove_button">
                              <form>
-                             <button class="delete_button" type="submit" name="deleteFromCartButton" value="'.$row['product_id'].'"><img src="../icons/remove%20icon.png"></button>
+                             <button class="delete_button" type="submit" name="deleteFromCartButton" value="' . $row['product_id'] . '"><img src="../icons/remove%20icon.png"></button>
                              </form>
                          </td>';
-                    echo' </tr>';
+                    echo ' </tr>';
                 }
 
-                $_SESSION['total_weight']=$totalWeight;
+                $_SESSION['total_weight'] = $totalWeight;
                 ?>
 
-            <tr>
-                <td class="total_column"></td>
-                <td class="info_column"><?php echo 'total quantity: '.$totalQuantity." pcs."?></td>
-                <td class="info_column"></td>
-                <td class="info_column"><?php echo 'sub total: '."₱ ".number_format($totalPrice, 2) ?></td>
-                <td class="info_column"></td>
-            </tr>
+                <tr>
+                    <td class="total_column"></td>
+                    <td class="info_column"><?php echo 'total quantity: ' . $totalQuantity . " pcs." ?></td>
+                    <td class="info_column"></td>
+                    <td class="info_column"><?php echo 'sub total: ' . "₱ " . number_format($totalPrice, 2) ?></td>
+                    <td class="info_column"></td>
+                </tr>
 
-            <?php
-            if(isset($_SESSION['shipping_fee'])){
-                $shippingFee=$_SESSION['shipping_fee'];
-                $grandTotal=$shippingFee+$totalPrice;
+                <?php
+                if (isset($_SESSION['shipping_fee'])) {
+                    $shippingFee = $_SESSION['shipping_fee'];
+                    $grandTotal = $shippingFee + $totalPrice;
 
-                echo '<tr>';
-                echo '<td class="total_column"></td>';
-                echo '<td class="info_column">'.'shipping fee: '."₱ ".number_format($shippingFee, 2).'</td>';
-                echo '<td class="info_column"></td>' ;
-                echo '<td class="info_column">'.'grand total: '."₱ ".number_format($grandTotal, 2).'</td>' ;
-                echo '<td class="info_column"></td>' ;
-                echo '</tr>';
-            }else{
-                echo '<tr>';
-                echo '<td class="total_column"></td>';
-                echo '<td class="info_column">'.'shipping fee: ---- '.'</td>';
-                echo '<td class="info_column"></td>' ;
-                echo '<td class="info_column">'.'grand total: ---- '.'</td>' ;
-                echo '<td class="info_column"></td>' ;
-                echo '</tr>';
-            }
+                    echo '<tr>';
+                    echo '<td class="total_column"></td>';
+                    echo '<td class="info_column">' . 'shipping fee: ' . "₱ " . number_format($shippingFee, 2) . '</td>';
+                    echo '<td class="info_column"></td>';
+                    echo '<td class="info_column">' . 'grand total: ' . "₱ " . number_format($grandTotal, 2) . '</td>';
+                    echo '<td class="info_column"></td>';
+                    echo '</tr>';
+                } else {
+                    echo '<tr>';
+                    echo '<td class="total_column"></td>';
+                    echo '<td class="info_column">' . 'shipping fee: ---- ' . '</td>';
+                    echo '<td class="info_column"></td>';
+                    echo '<td class="info_column">' . 'grand total: ---- ' . '</td>';
+                    echo '<td class="info_column"></td>';
+                    echo '</tr>';
+                }
 
-            ?>
+                ?>
 
-        </table>
+            </table>
 
-        <form action="shipping.php" method="post">
-            <button id="checkOutButton" type="submit" name="checkOutButton">Checkout</button>
-        </form>
+            <form action="shipping.php" method="post">
+                <button id="checkOutButton" type="submit" name="checkOutButton">Checkout</button>
+            </form>
 
-    </div>
+        </div>
+
+        <?php
+    }
+    ?>
 
 </section>
 
